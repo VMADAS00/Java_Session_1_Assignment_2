e0003b2367c91f2b392419c8cffbadd799ea2220
VMADAS00 <mvenky155@gmail.com>
2016-03-19 19:49:05 +0530

    Create Session_1_Declare_threeIntegers_Assignment_2.java

---
+++ Session_1_Declare_threeIntegers_Assignment_2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author venkey
+ *
+ */
+public class Session_1_Declare_threeIntegers_Assignment_2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,c,b=4;
+	}
+
+}
